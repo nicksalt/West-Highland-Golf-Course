de8c21e04120a2cdca17ad720a38c9ade74433fa
Nick Salt <nicksalt24@gmail.com>
2020-09-17 20:41:26 -0400

    Google API private

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { MAPS_API_KEY } from "../../private/variables";
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -13,7 +15,12 @@ export class ContactComponent implements OnInit {
   faPhone = faPhone;
   faEnvelope = faEnvelope;
   faFacebook = faFacebook
-  constructor() { }
+  mapsSource: SafeResourceUrl
+  constructor(private sanitizer: DomSanitizer) {
+    this.mapsSource = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.google.com/maps/embed/v1/place?key=" 
+    + MAPS_API_KEY + "&q=West+Highland+Golf+Course+Madoc+Ontario");
+    console.log(MAPS_API_KEY);
+   }
 
   ngOnInit(): void {
     document.body.scrollTop = 0; // For Safari
